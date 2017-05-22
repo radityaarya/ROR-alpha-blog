d9db0401023fd8f602616e43a5a9332a7ade19b8
radityaarya <raditya.arya@domikado.com>
2017-05-22 09:19:02 +0700

    fixing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,7 +16,7 @@ class ArticlesController < ApplicationController
       flash[:notice] = "An article was succesfully creatad"
       redirect_to article_path(@article)
     else
-      render article_path
+      render 'new'
     end
   end
 
