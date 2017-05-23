67436b9f0bc7df1f4ba9d9e6c254bbbb2a8339db
radityaarya <raditya.arya@domikado.com>
2017-05-23 10:26:55 +0700

    extract redundancy in controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,12 @@
 class ArticlesController < ApplicationController
+  before_action :set_article_with_params, only: [:show, :edit, :update, :destroy]
 
   def index
     @articles = Article.all
+  end
+
+  # Show Certain Article by ID
+  def show
   end
 
   # Show Form for New Input
@@ -22,11 +27,10 @@ class ArticlesController < ApplicationController
 
   # Edit Article
   def edit
-    @article = Article.find(params[:id])
+
   end
 
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       flash[:notice] = "An article was succesfully edited"
       redirect_to article_path(@article)
@@ -36,18 +40,17 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     flash[:notice] = "An article was succesfully deleted"
     redirect_to articles_path
   end
 
-  # Show Certain Article by ID
-  def show
-    @article = Article.find(params[:id])
-  end
 
   private
+    def set_article_with_params
+      @article = Article.find(params[:id])
+    end
+
     def article_params
       params.require(:article).permit(:title, :description)
     end
